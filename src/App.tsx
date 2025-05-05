abf84e5b47cba9bf261ac9dfe2d9b4d7e4f2f4ae
chris <c.f.oeser2@newcastle.ac.uk>
2025-05-05 19:14:36 +0100

    fix qr code bug

--- src/App.tsx
+++ src/App.tsx
@@ -124,7 +124,7 @@ const App: React.FC = () => {
               )}
             </div>
             
-            <QRCode />
+            {!imagePreview && <QRCode />}
 
             {predictionResult && (
               <>
